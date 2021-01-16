1eb079bc17fa1ad8f28a4fa4e2926205607cd998
Aniket <aniket.patanwal99@gmail.com>
2021-01-16 21:20:02 +0530

    gg

--- flask-crud-app/app.py
+++ flask-crud-app/app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+import os
 
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    
+    return render_template('base.html')
+
+
