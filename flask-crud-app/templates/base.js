9c8674d8c0f02f6776515a30f97b51a1a1328eae
AjEX <akashdanva007@gmail.com>
2021-01-17 18:05:39 +0530

    hahaha

--- flask-crud-app/templates/base.js
+++ flask-crud-app/templates/base.js
@@ -1,5 +1,5 @@
 const navSlide = () => {
-	const burgrer = document.querySelector('.burger');
+	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
 	//Toggle Nav
@@ -7,20 +7,19 @@ const navSlide = () => {
 		nav.classList.toggle('nav-active');
 
 		navLinks.forEach((link, index)=>{
-		if(link.style.annimation){
-			link.style.animation = '';
-		}
-		else{
-			link.style.animation = 'navLinkFade 0.5s ease forwards ${index/7 + 1.5}s';
-		}
-	});
+			if(link.style.animation){
+				link.style.animation = '';
+			}
+			else{
+				link.style.animation = navLinkFade 0.5s ease forwards ${index/7 + 1.5s};
+			}
+		});
+
+		//Burger Animation
+		burger.classList.toggle('toggle');
 	});
 }
 
 
 
-const main = ()=>{
-	navSlide();
-}
-
-main();
+navSlide();
